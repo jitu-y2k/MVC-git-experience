4016d79714c41e129daed56cd4f58a105ae7fde5
Jitendra Sharma <Jitendrasharma@hcl.com>
2022-05-17 20:26:52 +0000

    Addedd featuer1 class and Add mehod into it

---
+++ MVC-git-experience/feature1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_git_experience
+{
+    public class feature1
+    {
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
+    }
+}
